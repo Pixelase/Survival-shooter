d89fbd7a8a9d4684eb4b619158920e08584f40a0
Alexander Babaj <sustem34@gmail.com>
2014-11-11 18:39:45 +0300

    Scoring Points
    Добавлен скрипт ScoreManager.cs, который отвечает за подсчёт очков за убитых врагов.
    Добавлен непосредственно сам объект ScoreText, которые отвечает за отображение результата на экране.

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -71,6 +71,7 @@ public class EnemyHealth : MonoBehaviour
 		GetComponent <NavMeshAgent> ().enabled = false;
 		GetComponent <Rigidbody> ().isKinematic = true;
 		isSinking = true;
+		ScoreManager.score += scoreValue;
 		Destroy (gameObject, 2f);
 	}
 }
