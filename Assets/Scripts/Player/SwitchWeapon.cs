0c16fafe18037d5e16d53dc451987c2e04e3fe00
Alexander Babaj <sustem34@gmail.com>
2014-11-14 19:14:47 +0300

    Big gun kick the hell out of you
    "Вооружайся, вооружайся так, как никогда не вооружался"
    
    Что сделано:
    1) Добавлена модель пулёмёта, обработаны выстрелы, создан готовый prefab для удобства дальнейшей работы;
    2) В арсенал игрока добавлено мощнейшее оружие, иначе именуемое как "фонарик";
    3) Написан скрипт для переключения оружия (SwitchWeapon.cs);
    4) Всё оружие сгрупировано максимально удобным образом;

---
+++ Assets/Scripts/Player/SwitchWeapon.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchWeapon : MonoBehaviour {
+
+	public GameObject gun1;
+	public GameObject gun2;
+	public GameObject flashlight;	
+
+	// Use this for initialization
+	void Start ()
+	{
+		gun1.SetActive(true);
+		gun2.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Switch();
+	}
+
+	void Switch()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			Start();
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+		{	
+			gun1.SetActive(false);
+			gun2.SetActive(true);
+		}
+		if(Input.GetKeyDown(KeyCode.F))
+		{	
+			if(flashlight.active) flashlight.SetActive(false);
+			else flashlight.SetActive(true);
+		}
+	}
+
+}
