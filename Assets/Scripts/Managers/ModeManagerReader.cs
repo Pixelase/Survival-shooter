ea38c3f0215f55a246646938857f73595f17e9b1
Alexander Babaj <sustem34@gmail.com>
2014-11-16 22:18:52 +0300

    Added two difficulty mods
    Добавлено два уровня сложности: Normal(обычный) и Insanity(безумие);
    Добавлены скрипты для реализации уровней сложности: ModeManagerReader.cs и ModeManager.cs
    Для хранения значений используется файл Mode.ini: это сделано для передачи данных между сценами Menu и MainScene;

---
+++ Assets/Scripts/Managers/ModeManagerReader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+
+public class ModeManagerReader : MonoBehaviour
+{
+	EnemyManager []enemies;
+
+	void Start()
+	{
+		ReadMode();
+	}
+	
+	void ReadMode() 
+	{
+		try
+		{
+			enemies = GetComponents<EnemyManager>();
+			using (StreamReader sr = new StreamReader("Mode.ini"))
+			{
+				foreach(EnemyManager enemy in enemies)
+				{
+					enemy.spawnTime = int.Parse(sr.ReadLine());
+					enemy.deltaSpawnTime = enemy.spawnTime;
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("The file could not be read:");
+			Console.WriteLine(e.Message);
+		}
+	}
+
+}
--- Assets/Scripts/Menu/ModeManager.cs
+++ Assets/Scripts/Menu/ModeManager.cs
@@ -3,26 +3,22 @@ using System.Collections;
 
 public class ModeManager : MonoBehaviour
 {
-	EnemyManager enemyManager;
-	GameObject newObject;
-
-	void Awake()
-	{
-		enemyManager = GetComponent <EnemyManager> ();
-	}
-
 	public void Normal()
 	{
-
+		using (System.IO.StreamWriter file = new System.IO.StreamWriter("Mode.ini"))
+		{
+			file.WriteLine("3\n3\n10");
+		}
+		Application.LoadLevel("Option");
 	}
 
 	public void Insanity ()
 	{
-		
+		using (System.IO.StreamWriter file = new System.IO.StreamWriter("Mode.ini"))
+		{
+			file.WriteLine("2\n2\n6");
+		}
+		Application.LoadLevel("Option");
 	}
 
-	void Start ()
-	{
-		newObject = GameObject.Find ("EnemyManager");
-	}
 }
